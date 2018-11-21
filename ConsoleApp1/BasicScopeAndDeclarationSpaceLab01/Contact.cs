644f0c59b6dc8bc9f718bee6dc9c840ed52205f8
vienTM <thanminhvien@gmail.com>
2018-11-21 09:41:57 +0700

    add Project Basic Scope 01

---
+++ ConsoleApp1/BasicScopeAndDeclarationSpaceLab01/Contact.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicScopeAndDeclarationSpaceLab01
+{
+    public class Contact
+    {
+        public int age;
+
+        public void FuncA()
+        {
+            age = 18;
+            Console.WriteLine(age);
+        }
+
+        public void FuncB()
+        {
+            int age;
+            age = 21;
+            Console.WriteLine(age);
+        }
+    }
+}
---
+++ ConsoleApp1/BasicScopeAndDeclarationSpaceLab01/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicScopeAndDeclarationSpaceLab01
+{
+    class Program
+    {
+        // Scope and Declaration Space
+        /* http://www.informit.com/articles/article.aspx?p=1609145&seqNum=4&fbclid=IwAR1Qe14M6J3aL6o2GoLAGsB03t85Y4a88lQtIVN0nybobW8f0W8TN-9XXtc
+        */
+
+        static void Main(string[] args)
+        {
+            Contact ct = new Contact();
+            ct.age = 20;
+            Console.Write("Function A: ");
+            ct.FuncA();
+
+            Console.Write("Function B: ");
+            ct.FuncB();
+
+            Console.WriteLine("ct.age = {0}", ct.age);
+
+            Console.ReadLine();
+        }
+    }
+}
